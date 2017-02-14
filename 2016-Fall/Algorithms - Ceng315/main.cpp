c775dc327af573903bc8ae9fbef55d4e524d95ef
Bahar Aydemir <baharay@users.noreply.github.com>
2017-02-15 01:31:59 +0200

    Create main.cpp

---
+++ 2016-Fall/Algorithms - Ceng315/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include "th2.h"
+using namespace std;
+int main(int argc, char *argv[]){    
+
+
+	cout << dfa_equality(argv[1], argv[2]);
+  
+    return 0;
+}
